d104bf683015bd764080bd86765072b17434a217
YoussefElsafi <youssefelsafi2012@gmail.com>
2024-09-11 19:07:19 +0300

    Configuration for BatchBot (v2.0.0)

--- system/config.py
+++ system/config.py
@@ -12,14 +12,17 @@ server_name = "EPIC SERVER" # Replace with your server name
 # Config Settings
 ai_toggle = False # Start automatically with ai toggle on or off
 pro = False # Options (True (Gemini 1.5 Pro), True+ (gemini-1.5-pro-exp-0827), False (Gemini 1.5 Flash))
-online_toggle = False # will activate always on bot (Under-Dev)
 limit_history = False  # Use a limited conversation history
 history_limit = 100     # Limit for conversation history (if limit_history is True)
-show_time = True # Makes the bot able to know what is the time and date and also the time that the messages was sent
+show_time = False # Makes the bot able to know what is the time and date and also the time that the messages was sent
+history_channel_toggle = True  # Enable or disable per-channel history
+embed_colors = 0x00ff00 # Embed color
+show_tokens_at_startup = False # Shows the tokens and the api keys on the terminal at startup
 
 # Models Settings
-Image_Generator_Model = "stabilityai/stable-diffusion-xl-base-1.0" # model of the image generator
-DEFAULT_MUSIC_MODEL = "facebook/musicgen-small" # Model of the music generation
+Image_Generator_Model = "stabilityai/stable-diffusion-xl-base-1.0" # Model of the image generator | You might need to reinvite the bot to fully update the model
+DEFAULT_MUSIC_MODEL = "facebook/musicgen-small" # Model of the music generation | You might need to reinvite the bot to fully update the model
+Object_Detection_Model = "facebook/detr-resnet-50" # Model of the object detection
 custom_model = False # Use a custom google gemini model
 custom_model_name = "gemini-1.5-flash" # Name of the custom model
 custom_model_tokens = 1048576 # Tokens of the custom model
@@ -38,7 +41,6 @@ sys_security = [
     {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
-
 if custom_model:
     max_tokens = custom_model_tokens
 else:
